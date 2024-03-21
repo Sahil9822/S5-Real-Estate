02facdb676fc88002666709b8fd56724057224b7
Sahil Pawale <97694477+Sahil9822@users.noreply.github.com>
2024-03-21 12:33:15 +0530

    Update store.js

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -2,8 +2,10 @@ import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import userReducer from './user/userSlice';
 import { persistReducer, persistStore } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
+import themeReducer from './theme/themeSlice';
 
-const rootReducer = combineReducers({ user: userReducer });
+
+const rootReducer = combineReducers({ user: userReducer, theme: themeReducer, });
 const persistConfig = { key: 'root', storage, version: 1 };
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
